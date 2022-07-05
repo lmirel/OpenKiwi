cc071bd995ecd493931efba02f6a11c58e685dd0
mirel <mirel.t.lazar@gmail.com>
2022-07-05 21:03:42 +0100

    allow it to run on dumber systems

--- kiwi/systems/__init__.py
+++ kiwi/systems/__init__.py
@@ -24,7 +24,7 @@ from pkgutil import iter_modules
 
 # iterate through the modules in the current package
 package_dir = Path(__file__).resolve().parent
-for (_, module_name, _) in iter_modules([package_dir]):
+for (_, module_name, _) in iter_modules([str(package_dir)]):
     # import the module and iterate through its attributes
     module = import_module(f"{__name__}.{module_name}")
     # for attribute_name in dir(module):
